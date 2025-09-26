c33b2463bfa2e130ad931facdffca80c8a9717bb
Sidakov21 <amirsidacov09@gmail.com>
2025-09-26 12:33:21 +0300

    • реализация логики

--- ECommerceApp.Core/Class1.cs
+++ ECommerceApp.Core/Class1.cs
@@ -2,5 +2,22 @@
 
 public class Class1
 {
+        public class Product
+    {
+        public int Id { get; set; }          // уникальный идентификатор
+        public string Name { get; set; }     // название
+        public decimal Price { get; set; }   // цена
 
+        public Product(int id, string name, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}: {Name} - {Price}₽";
+        }
+    }
 }
--- ECommerceApp.Data/Class1.cs
+++ ECommerceApp.Data/Class1.cs
@@ -1,6 +1,28 @@
-﻿namespace ECommerceApp.Data;
+﻿using ECommerceApp.Core.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ECommerceApp.Data;
 
 public class Class1
 {
+        public class ProductRepository
+    {
+        private readonly List<Product> _products = new();
 
+        public void AddProduct(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            return _products;
+        }
+
+        public Product? GetById(int id)
+        {
+            return _products.FirstOrDefault(p => p.Id == id);
+        }
+    }
 }
